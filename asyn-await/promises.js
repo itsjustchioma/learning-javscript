b92687eedfbf344b313da9864ea46998825aa7b6
Chioma Audrey Uche-Nwosu <74658446+itsjustchioma@users.noreply.github.com>
2022-11-12 21:38:24 +0100

    Update promises.js

--- asyn-await/promises.js
+++ asyn-await/promises.js
@@ -1,1 +1,27 @@
+"use strict";
+
+// PROMISES
+const myPromise = new Promise((resolve, reject) => {
+    const rand = Math.floor(Math.random() * 2);
+    if(rand ==0){
+        resolve();
+    }
+    else{
+        reject();
+    };
+});
+
+myPromise
+    .then(() => console.log('Success'))
+    .catch(() => console.error('Something went wrong'))
+
+
+// FETCH WITH PROMISES 
+fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.error(err));
+
+
+// ASYNC AWAIT
 
